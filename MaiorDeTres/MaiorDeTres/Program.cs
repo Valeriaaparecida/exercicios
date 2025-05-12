c1db5f90ab4ca82cfb37182006767310b192ddc4
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-05-12 19:11:28 -0300

    adicionando exercicios

---
+++ MaiorDeTres/MaiorDeTres/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("DIgite o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("DIgite o segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("DIgite o terceiro número: ");
+int num3 = int.Parse(Console.ReadLine());
+int maior;
+if (num1 > num2 && num1 > num3)
+{
+    maior = num1;
+}
+else if (num2 > num1 && num2 > num3)
+{
+    maior = num2;
+}
+else
+{
+    maior = num3;
+}
+Console.WriteLine($"O maior número é: {maior}");
---
+++ Par_Ou_Impar/Par_Ou_Impar/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite um número: ");
+int A = int.Parse(Console.ReadLine());
+
+
+if (A % 2 == 0)
+{
+    Console.WriteLine($"O número {A} é um número par");
+}
+else
+{
+    Console.WriteLine($"O número {A} é um número ímpar");
+}
