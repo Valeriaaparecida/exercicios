132a7e0e4d7fa770d6d12117b64dbb748e706f71
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-05-12 18:46:05 -0300

    commit inicial

---
+++ uri 1072/uri 1072/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, i, x, dentro,fora;
+            dentro = 0;
+            fora = 0;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= N; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+            } 
+            Console.WriteLine(dentro + "in");
+            Console.WriteLine(fora + "out");
+        }
+    }
+}
---
+++ uri 1131/uri 1131/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int vitoriaInter = 0;
+            int vitoriaGremio = 0;
+            int empate = 0;
+            int novoGrenal = 1;
+            int grenais = 0;
+            string res;
+
+            while (novoGrenal == 1)
+            {
+                string[] gols = Console.ReadLine().Split(' ');
+                int inter = int.Parse(gols[0]);
+                int gremio = int.Parse(gols[1]);
+
+                if (inter > gremio)
+                {
+                    vitoriaInter ++;
+                } else if (inter < gremio)
+                {
+                    vitoriaGremio ++;
+                }
+                else
+                {
+                    empate ++;
+                }
+                grenais ++;
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                novoGrenal = int.Parse(Console.ReadLine());
+
+            }
+            if (vitoriaInter > vitoriaGremio)
+            {
+                res = "Inter venceu mais";
+            } else if (vitoriaGremio > vitoriaInter)
+            {
+                res = "Gremio venceu mais";
+            } else
+            {
+                res = "Houve empate";
+            }
+
+            Console.WriteLine(grenais + " grenais");
+            Console.WriteLine("Inter: " + vitoriaInter);
+            Console.WriteLine("Gremio: " + vitoriaGremio);
+            Console.WriteLine("Empates: " + empate);
+            Console.WriteLine(res);
+        }
+    }
+}
